b9946a5324abe1b41bab17937548416e1d377f19
Thanhcm12345955 <135189830+Thanhcm12345955@users.noreply.github.com>
2023-10-24 00:35:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,41 +1,4 @@
 <?php
-class Connect{
-    public $server;
-    public $dbname;
-    public $uname;
-    public $pass;
-    public function __construct(){
-        $this->server = "localhost";
-        $this->dbname = "shop_gcc200193";
-        $this->uname = "root";
-        $this->pass = "";
-    }
-    function connectToMySQL():mysqli{
-        $conn = new mysqli($this->server,$this->uname,$this->pass,$this->dbname);
-
-        if($conn->connect_error){
-            die("Failed ".$conn->connect_error);
-        }else{
-            // echo "Connect!";
-        }
-        return $conn;
-    }
-
-    //option 2 : PDO
-    function connectToPDO():PDO{
-        try{
-            $conn = new PDO("mysql:host=$this->server;dbname=$this->dbname",$this->uname,
-            $this->pass);
-            // echo "Connect! PDO";
-        }catch(PDOException $e){
-            print("Failed ".$e->getMessage());
-        }
-        return $conn;
-    }
- 
-}
-$c = new Connect();
-$c->connectToMySQL();
-echo "<br>";
-$c->connectToPDO();
+ $Connet = mysql_connect("tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","n61ng5kc3pzx5zy5","w0drw70obwimr3ll","vta9ll0wskbeykiw") or die("Error".mysql_error($Connet));
+ mysql_query($Connet,'SET NAME "utf8"');
 ?>
