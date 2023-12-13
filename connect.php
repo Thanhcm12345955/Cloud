02d8f6e290eec49110222b1d27795e81b8f5ab2a
Thanhcm12345955 <135189830+Thanhcm12345955@users.noreply.github.com>
2023-12-13 18:03:37 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,10 +5,10 @@ class Connect{
     public $uname;
     public $pass;
     public function __construct(){
-        $this->server = "exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-        $this->dbname = "lldsf49tk1u1zkvi";
-        $this->uname = "iw67alqykvjxiuxx";
-        $this->pass = "tqtiut2yk8ote0wj";
+        $this->server = "d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $this->dbname = "z0mawdcqcn6strv9";
+        $this->uname = "c5jqukpgfykwp8d2";
+        $this->pass = "idoy019pcpwg28bi";
     }
     function connectToMySQL():mysqli{
         $conn = new mysqli($this->server,$this->uname,$this->pass,$this->dbname);
